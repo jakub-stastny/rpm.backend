ddcb5d5d55ce4f2d4ac2156888af2922164eecc2
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2021-06-21 01:18:47 +0000

    Tangle deps.edn from main.org

--- .env/emacs.el
+++ .env/emacs.el
@@ -1,21 +1,7 @@
-; Don't forget to see README.md in this directory
+; Don't forget to see README.org in this directory
 ; in order to install required packages using literate DevOps.
 
 (add-to-list 'load-path (expand-file-name "~/.zsh/environments/elisp"))
 
-(load "basic")
-
-(my/info "Loading project-specific configuration.")
-
+(load "literate-programming")
 (load-theme 'adwaita)
-
-; Support for Evil mode movement in 3rd party packages, such as Magit.
-(evil-collection-init)
-
-(defun my/local/tangle-org-mode-files ()
-  (my/info "Running my/local/tangle-org-mode-files.")
-  (org-babel-tangle))
-
-(add-hook 'org-mode-hook
-  (lambda ()
-    (add-hook 'after-save-hook 'my/local/tangle-org-mode-files nil nil)))
