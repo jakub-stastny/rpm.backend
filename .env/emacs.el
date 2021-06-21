d6eb7201465b2c5961a0dc88bc82374143d2c380
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2021-06-21 01:31:07 +0000

    Reorganise .env, ignore everything but README.org

--- .env/emacs.el
+++
@@ -1,7 +0,0 @@
-; Don't forget to see README.org in this directory
-; in order to install required packages using literate DevOps.
-
-(add-to-list 'load-path (expand-file-name "~/.zsh/environments/elisp"))
-
-(load "literate-programming")
-(load-theme 'adwaita)
